11920370a8144b467a9a80ad5bca596f26b64859
k-marczak <k_marczak@outlook.com>
2021-03-12 20:57:07 +0100

    update projects

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -3,7 +3,7 @@ import Icon1 from '../../images/projekt1.png'
 import Icon2 from '../../images/project1.png'
 import Icon3 from '../../images/project-covid.png'
 import Icon4 from '../../images/snake.png'
-import Icon5 from '../../images/FormValidation.png'
+import Icon5 from '../../images/Screenshot_1.png'
 import './style.css'
 import { GrGithub } from 'react-icons/gr';
 import { GiNetworkBars } from 'react-icons/gi';
@@ -82,14 +82,14 @@ const Services = () => {
                 </ServicesCard>
                 <ServicesCard>
                     <ServicesIcon src={Icon5} />
-                    <ServicesH2>Form validation</ServicesH2>
-                    <ServicesP>Simple Form using HTML CSS JS. </ServicesP>
+                    <ServicesH2>Youtube Stats</ServicesH2>
+                    <ServicesP>Youtube application which showing you Tags and general Info about specific video.</ServicesP>
 
                     <LinksContainer>
-                        <a href="https://github.com/k-marczak/Form-validation-js" className="link" style={{ textDecoration: 'none'}} target="_blank" aria-label="Twitter">
+                        <a href="https://github.com/k-marczak/yt-stats" className="link" style={{ textDecoration: 'none'}} target="_blank" aria-label="Twitter">
                                     Github <GrGithub />
                         </a>
-                        <a href="https://validation-formm.netlify.app/" className="link" style={{ textDecoration: 'none', marginBottom: '10px'}} target="_blank" aria-label="Twitter">
+                        <a href="https://youtube-stats1.netlify.app/" className="link" style={{ textDecoration: 'none', marginBottom: '10px'}} target="_blank" aria-label="Twitter">
                                     Live <GiNetworkBars />
                         </a>
                     </LinksContainer>
