7e30cebb22bf983debbcea41fecace844af4ec8f
k_marczak <k_marczak@outlook.com>
2022-01-26 16:50:58 +0100

    im here!

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,15 @@
-import firebase from 'firebase'
+import firebase from "firebase";
 
 var firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyDUswAa4kNXTdhawqxUEJ-jbnUwL_jGWtY",
-    authDomain: "react-contact-form-b134b.firebaseapp.com",
-    databaseURL: "https://react-contact-form-b134b-default-rtdb.firebaseio.com",
-    projectId: "react-contact-form-b134b",
-    storageBucket: "react-contact-form-b134b.appspot.com",
-    messagingSenderId: "814068586217",
-    appId: "1:814068586217:web:334843eba774340fd40278"
-  });
+  apiKey: "AIzaSyDUswAa4kNXTdhawqxUEJ-jbnUwL_jGWtY",
+  authDomain: "react-contact-form-b134b.firebaseapp.com",
+  databaseURL: "https://react-contact-form-b134b-default-rtdb.firebaseio.com",
+  projectId: "react-contact-form-b134b",
+  storageBucket: "react-contact-form-b134b.appspot.com",
+  messagingSenderId: "814068586217",
+  appId: "1:814068586217:web:334843eba774340fd40278",
+});
 
+var db = firebaseApp.firestore();
 
-  var db = firebaseApp.firestore();
-
-
-  export { db };
+export { db };
