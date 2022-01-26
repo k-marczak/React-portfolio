2eb07f913c3535dd95cd231af0493ac1c520477e
k_marczak <k_marczak@outlook.com>
2022-01-26 16:58:14 +0100

    s

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import React from 'react'
-import './App.css'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './pages';
-
-
-
-
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages";
 
 function App() {
   return (
